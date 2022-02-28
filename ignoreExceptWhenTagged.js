d10d251d0e6d7b79e0d212520c47a72de8b818a1
Fritz <78668750+Fritz-02@users.noreply.github.com>
2022-02-28 10:20:00 -0800

    Updated to use dggFunctions.js

--- ignoreExceptWhenTagged.js
+++ ignoreExceptWhenTagged.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ignore Except When Tagged
 // @namespace    https://www.destiny.gg/
-// @version      1.1.0
+// @version      1.2.0
 // @description  Does what /ignore does, except you'll see messages you're tagged in.
 // @author       Fritz
 // @include      /https?:\/\/www\.destiny\.gg\/embed\/chat/
@@ -9,60 +9,14 @@
 // @updateURL    https://github.com/Fritz-02/dgg-scripts/raw/main/ignoreExceptWhenTagged.js
 // @icon         https://www.google.com/s2/favicons?domain=destiny.gg
 // @grant        none
+// @require      https://raw.githubusercontent.com/Fritz-02/dgg-scripts/main/dggFunctions.js
 // ==/UserScript==
 
 
-const storage = {};
-storage["iewt-ignored"] = window.localStorage.getItem("iewt-ignored") ? JSON.parse(window.localStorage.getItem("iewt-ignored")) : [];
-storage["iewt-harshIgnored"] = window.localStorage.getItem("iewt-harshIgnored") ? JSON.parse(window.localStorage.getItem("iewt-harshIgnored")) : [];
+createStorageItem("iewt-ignored", []);
+createStorageItem("iewt-harshIgnored", []);
 
-//console.log(storage);
-
-
-// Settings
-const settings = document.querySelector("#chat-settings-form");
-
-function createSetting(setting) {
-    let settingGroup = document.createElement("div");
-    settingGroup.className = "form-group row";
-
-    let settingLabel = document.createElement("label");
-    settingLabel.innerHTML = setting.text;
-    if (setting.hint) {settingLabel.title = setting.hint};
-    settingGroup.appendChild(settingLabel);
-
-    let settingInput;
-    settingInput = document.createElement("textarea");
-    settingInput.style.resize = "vertical";
-    settingInput.className = "form-control";
-    settingInput.placeholder = setting.placeholder;
-
-    settingInput.value = storage[setting.storageName] == "[]" ? "" : storage[setting.storageName];
-
-    settingInput.addEventListener("change", () => {
-        let val = settingInput.value.toLowerCase().replaceAll(" ", "").split(",").filter(String);
-        //console.log(val);
-        if (settingInput.value.length > 0) {
-            storage[setting.storageName] = val;
-            window.localStorage.setItem(
-                setting.storageName, JSON.stringify(val)
-            );
-        } else {
-            storage[setting.storageName] = [];
-            window.localStorage.setItem(
-                setting.storageName, JSON.stringify(storage[setting.storageName])
-            );
-        }
-    });
-    settingGroup.appendChild(settingInput);
-    settings.appendChild(settingGroup);
-}
-
-let settingHeading = document.createElement("h4");
-settingHeading.innerHTML = "ignore except when tagged";
-settings.appendChild(settingHeading);
-
-
+newSettingGroup("ignore except when tagged");
 createSetting({
     "text": "Ignored chatters",
     "type": "textarea",
